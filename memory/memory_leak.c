05ab5b27a7a9f2af9e9e8a3e162d668d41dc622c
Carlos Daniel <cd38668cd@gmail.com>
2026-06-28 23:01:16 -0300

    Memory leak implement in code existing 2 memory leak

---
+++ memory/memory_leak.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Este codigo e proposto para demonstrar um vazamento de memoria 
+  A nao chama free() para liberar a memória alocada, portanto, ocorre um vazamento de memoria 
+  E nao funçao main essa funçao e chamada 3 vezes, Quando a função termina,
+  LeakVar deixa de existir porque era uma variável local da stack.
+  A memória continua ocupada no heap, mas não existe mais nenhum ponteiro para ela
+  
+  */
+
+void memory_leak_example() {
+  
+    int *leakVar = (int *)malloc(sizeof(int));
+    if (leakVar == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return;
+    }
+    *leakVar = 42;
+
+   // free(leakVar);
+
+}
+
+
+int main() {
+    memory_leak_example();
+
+
+    char* nome = (char*)malloc(50 * sizeof(char));
+    if (nome == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");  
+        return 1;
+    }
+    // free(nome);
+
+    return 0;
+}
