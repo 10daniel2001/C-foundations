3a472c73aabfd944c43bf8eceba1d17491b824ef
10daniel2001 <cd38668cd@gmail.com>
2026-07-13 21:31:56 -0300

    updating coments for binary search

--- data_strutures/bin_search.c
+++ data_strutures/bin_search.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Busca binaria*/
+/*Busca binaria
+  binary search
+  
+  */
 
 void search(int vetor[], int n, int target){
     int i, j;
-
+   
     for ( i = 0; i < n; i++)
     {
         if (vetor[i] == target)
@@ -15,12 +18,16 @@ void search(int vetor[], int n, int target){
         }
         
     }
+    // Function to perform a binary search on an array to find a target value.
+    // Função para realizar uma busca binária em um array para encontrar um valor alvo.
     
 }
 
 int main(){
     int veto[] = {2,5,4,8,9,7};
     int n = sizeof(veto)/sizeof(veto[0]);
+    // Function to calculate the size of the array.
+    // Função para calcular o tamanho do array.
     int target = 2;
 
     printf("Busca binaria\n");
