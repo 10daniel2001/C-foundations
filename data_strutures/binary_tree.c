fd8982463868d93ce50ce149ec943c995e81f205
10daniel2001 <cd38668cd@gmail.com>
2026-07-16 22:21:11 -0300

    coments

--- data_strutures/binary_tree.c
+++ data_strutures/binary_tree.c
@@ -3,7 +3,7 @@
 
 /* Binary Tree or arvore binaria
 *
-*
+*Representa uma estrutura de dados em que cada nó tem no máximo dois filhos, geralmente chamados de filho esquerdo e filho direito.
 *
 *
 *
@@ -17,6 +17,10 @@ struct Node {
 
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    //Craiando um novo nó e alocando memória para ele
+    // Inicializando os valores do nó
+    // Definindo o valor do nó, e os ponteiros para os filhos como NULL
+    // retornando o novo nó criado
     newNode->data = data;
     newNode->left = NULL;
     newNode->right = NULL;
