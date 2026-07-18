1d8b02d8865554371d1500e217905bbf6ac0eeac
10daniel2001 <cd38668cd@gmail.com>
2026-07-17 22:55:43 -0300

    testando no main e nao finalizando ainda

--- data_strutures/binary_tree.c
+++ data_strutures/binary_tree.c
@@ -35,7 +35,30 @@ struct Node* createNode(int data) {
 }
 
 void em_ordem(struct Node* n){
-    printf("Dataa .. %d\n", n->data);
+    printf("Data .. %d\n", n->data);
     em_ordem(n->left);
     em_ordem(n->right);
+}
+
+void liberar_arvore(struct Node* n) {
+    if (n == NULL) {
+        return;
+    }
+    liberar_arvore(n->left);
+    liberar_arvore(n->right);
+    free(n);
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    em_ordem(root);
+
+   
+    liberar_arvore(root);
+    return 0;
 }
