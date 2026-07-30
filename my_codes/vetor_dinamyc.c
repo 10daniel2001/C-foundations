10849b94a49da20eba1869481fa07b8a17153ea7
10daniel2001 <cd38668cd@gmail.com>
2026-07-30 18:28:36 -0300

    Creating arrays dinamic with malloc the type char

--- my_codes/vetor_dinamyc.c
+++ my_codes/vetor_dinamyc.c
@@ -9,16 +9,34 @@
 
 int main(){
 
+  //arrays the number int
   int* new_memory = (int*)malloc(5 * sizeof(int));
+
+  //arrays the character char, dinamic!
+  char* new_character = malloc(10 * sizeof(char));
+
+for (int i = 0; i < 10; i++)
+{
+    new_character[i] = 'A' + i;
+    printf("%c\n", new_character[i]);
+}
+  
+
+  
+  //new_memory[0] = 10;
+  //new_memory[1] = 20;
+  //new_memory[3] = 30;
+  //Exemple
 
   for (int i = 0; i < 5; i++)
   {
-    new_memory[i] = i+5;
+    new_memory[i] = i+10;
     printf("%d\n", new_memory[i]);
   }
 
 
 
+  free(new_character);
   free(new_memory);
   
     return 0;
