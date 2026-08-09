fa4d7307f6e4c4d34eed6b9ac1ff503a1459eb8b
10daniel2001 <cd38668cd@gmail.com>
2026-08-09 14:40:36 -0300

    Iniciando exemplo de abri processoas em C

---
+++ my_codes/initiating processes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Initiating Processes 
+   Abrindo Processos 
+   
+*/
+
+int main(int argc, char *argv[]) {
+    printf("Iniciando o processo...\n");
+    
+    int pid, i;
+
+    for (i = 0; i < 5; i++) {
+       
+    }   
+
+}
