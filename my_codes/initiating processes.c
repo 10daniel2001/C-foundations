a8d9124b858d74dbf82819fd8940f6f91b606855
10daniel2001 <cd38668cd@gmail.com>
2026-08-09 15:00:45 -0300

    Codigo simples e basico sobre o uso de fork em C, abrindo ls para lista diretorio data para mostra data, e o ultimo abre o brave navegador

--- my_codes/initiating processes.c
+++ my_codes/initiating processes.c
@@ -21,31 +21,46 @@
 int main(int argc, char **argv, char* envp[]) {
 
     int pid, i;
+    
+    // Loop to create 3 child processes
+    // Loop para criar 3 processos filhos
+    for (i=1; i<=3; i++) {
+    
+        pid = fork();
+         // check if fork() failed
 
-for (i=1; i<=3; i++) {
+    if (pid < 0) {
+        // Fork failed
+        // Fork falhou
+        printf("Algo deu errado!!!\n");
+    return 0;
+    
+    
+    }
 
-    pid = fork();
-    // check if fork() failed
-if (pid < 0) {
-
-    printf("Algo deu errado!!!\n");
-return 0;
-}
-
-if (pid == 0) { // Processo filho
-
-
- if (i == 1)
-
-    execve("/usr/bin/ls", argv, envp);
-if (i == 2)
-
-    execve("/usr/bin/date", argv, envp);
-if (i == 3)
-    execve("/usr/bin/brave-browser", argv, envp);
-}
-else // Processo pai
-    waitpid(pid, NULL, 0);
-}
+    if (pid == 0) { // Processo filho
+        // Child process
+        
+        if (i == 1)
+        // Execute the "ls" command
+        // Executa o comando "ls" do terminal tradicional do Linux
+        execve("/usr/bin/ls", argv, envp);
+    if (i == 2)
+        // Execute the "date" command
+        // Executa o comando "date" do terminal tradicional do Linux
+        execve("/usr/bin/date", argv, envp);
+    if (i == 3)
+        // Execute the "brave-browser" command
+        // Executa o comando "brave-browser" do terminal tradicional do Linux
+        execve("/usr/bin/brave-browser", argv, envp);
+    }
+    else // Processo pai
+    {
+        // Parent process
+        printf("Processo pai aguardando o filho %d terminar...\n", i);
+        waitpid(pid, NULL, 0);
+        printf("Filho %d terminou.\n", i);
+        
+    }
 
 }
