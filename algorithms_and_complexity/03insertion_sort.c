315cb5af8a559be9102f5976e93f01edfc8dae42
10daniel2001 <cd38668cd@gmail.com>
2026-08-11 22:06:29 -0300

    funçao de comparacao de indices, e decremento

--- algorithms_and_complexity/03insertion_sort.c
+++ algorithms_and_complexity/03insertion_sort.c
@@ -10,10 +10,18 @@ void insertion_sort(int vetor[], int n){
     if (n <= 1)
     return;
 
-    
+    insertion_sort(vetor, n-1);
 
+    int key = vetor[n-1];
+    int jj = n-2;
 
-    
+    while (jj >= 0 && vetor[jj] > key )
+    {
+        vetor[jj+1] = vetor[jj];
+        jj--;
+    }
+   
+    vetor[jj+1] = key;
     
 }
 
