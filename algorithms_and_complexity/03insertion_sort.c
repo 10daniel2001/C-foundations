6ed1e36caff709999801d0cc1e85cfca5790b510
10daniel2001 <cd38668cd@gmail.com>
2026-08-11 22:00:48 -0300

    implementando condiçao de parada, para que nao aja estouro

--- algorithms_and_complexity/03insertion_sort.c
+++ algorithms_and_complexity/03insertion_sort.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+
+/*Recursuve insertion sort, um algoritmo de ordenaçao 
+
+*/
+
+
+void insertion_sort(int vetor[], int n){
+    if (n <= 1)
+    return;
+
+    
+
+
+    
+    
+}
 
