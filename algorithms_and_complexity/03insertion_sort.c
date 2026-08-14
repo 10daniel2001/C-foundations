4fdaf22700fe355c5bd47a770ff154c22a8ca62d
10daniel2001 <cd38668cd@gmail.com>
2026-08-13 21:12:05 -0300

    Explicando a ligica com comentarios, mais linhas

--- algorithms_and_complexity/03insertion_sort.c
+++ algorithms_and_complexity/03insertion_sort.c
@@ -14,21 +14,22 @@ void insertion_sort(int vetor[], int n){
 
     insertion_sort(vetor, n-1);
 
-    int key = vetor[n-1]; // total de indices e 5 entao 4-1 = 3, key [3]
+    int key = vetor[n-1]; // total de indices e 4 entao 4-1 = 3, key [3]
     int jj = n-2;   // jj = 4-2 = 2
 
-    // Exemplos [30,50,40,20,10] Elementos
+    // Exemplos [30,50,40,20,10] Elementos 
     // Indice   [ 0, 1, 2, 3, 4 ]  
     while (jj >= 0 && vetor[jj] > key )
     {   // jj >= 0 i is true end vetor[40] > key = 20 , elementos 
         // true 
         vetor[jj+1] = vetor[jj];
-       //vetor[4] = veteor[3] 
+       //vetor[3] = veteor[2] 
         jj--;
+        // jj _ 1-2 = 1
     }
    
     vetor[jj+1] = key;
-    
+    //vetor[3] = key = 20
 }
 
 void imprimir(int vet[], int n){
