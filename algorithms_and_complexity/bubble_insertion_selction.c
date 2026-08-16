ded6e649e8bee37bf2b93769d88d65e8ec3fd8b8
10daniel2001 <cd38668cd@gmail.com>
2026-08-16 11:18:45 -0300

    implementaçao bubble

--- algorithms_and_complexity/bubble_insertion_selction.c
+++ algorithms_and_complexity/bubble_insertion_selction.c
@@ -9,8 +9,33 @@ struct Cars{
     int id;
 };
 
+//Funçao para realizar trocas para o selection sort, bubble
 void trocar(struct Cars* a, struct Cars *b){
     struct Cars temp = *a;
     *a = *b;
     *b = temp;
+}
+
+//Bubble sort
+void bubble_sort(struct Cars list[], int n){
+    int i, j;
+
+        // For externo para percorre a lista
+
+    for ( i = 0; i < n; i++)
+    {
+        // For interno para ordenar
+           
+        for ( j = 0; j < n-1-i; j++)
+        {
+           if (list[j].id > list[j+1].id)
+           {
+              trocar(&list[j], &list[j+1]);
+              // Chamando funçao trocar para realizar as efetivas trocas
+           }
+           
+        }
+        
+    }
+    
 }
