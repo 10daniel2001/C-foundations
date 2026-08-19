515cccbfe34415d7260f45047515de8c5d5afd80
carlos daniel <cd38668cd@gmail.com>
2026-08-19 19:41:38 -0300

    A funçao merge_sort finaliza, e nela usa 3 chamadas de funçao, uma sendo recursiva, é outras para ordenaçao

--- algorithms_and_complexity/merge_sort.c
+++ algorithms_and_complexity/merge_sort.c
@@ -4,14 +4,14 @@
   No fim problemas dividos e partes menores, ultilizando recursao, e uma memoria extra 
 */
 
-void merge_sort(int vetor[], int inicio, int meio, int fim){
-	int n1 = meio - fim + 1;
+void merge(int vetor[], int inicio, int meio, int fim){
+	int n1 = meio - inicio + 1;
         int n2 = fim - meio;
  // Declarei 2 variaveis para uso, n1 é n2, nelas contem dados do meio subtraindo com fim + 1, é e meio - fim
 	int vetesquerdo[n1]; // Armazena o lado esquerdo do vetor ja dividido
         int vetdireito[n2];  // Armazena o lado direito do vetor tambem ja ordenando
 
-        for(int i = 0; i < n1; i++) vetesquerdo[i] = vetor[inicio + 1];
+        for(int i = 0; i < n1; i++) vetesquerdo[i] = vetor[inicio + i];
         for(int j = 0; j < n2; j++) vetdireito[j] = vetor[meio+1+j]; 
 
         int i = 0, j = 0, k = inicio;
@@ -24,11 +24,29 @@ void merge_sort(int vetor[], int inicio, int meio, int fim){
                   vetor[k] = vetdireito[j++];
     
                 }// fim do if 
-
+   	  k++;
+            
         }//Fim do while
-   	k++;
+   
 	
     	while(i < n1) vetor[k++] = vetesquerdo[i++];
  	while(j < n2) vetor[k++] = vetdireito[j++];
 
 }//fim da funçao
+
+// Lembrando este codigo esta sendo feito com o editor de texto nano do linux
+// por isso o uso de comentarios ao fim das chaves
+
+
+void merge_sort(int vetor[], int inicio, int fim){
+     if (inicio < fim){
+        int meio = inicio + (fim - inicio) / 2;
+		
+	merge_sort(vetor, inicio, fim);
+	merge_sort(vetor, meio + 1, fim);
+  
+	merge(vetor, inicio, meio fim);  
+     }// fim if
+}//fim da funçao
+
+
