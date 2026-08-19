20003adefb101c8ccef92ea6df956f0f234f73de
carlos daniel <cd38668cd@gmail.com>
2026-08-19 19:28:41 -0300

    Iniciand explicaçao de logica

--- algorithms_and_complexity/merge_sort.c
+++ algorithms_and_complexity/merge_sort.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+/*Um algoritmo ja poderoso para ordençao usando recursao,e devidindo em partes menores
+  No fim problemas dividos e partes menores, ultilizando recursao, e uma memoria extra 
+*/
+
 void merge_sort(int vetor[], int inicio, int meio, int fim){
 	int n1 = meio - fim + 1;
         int n2 = fim - meio;
-
-	int vetesquerdo[n1];
-        int vetdireito[n2];
+ // Declarei 2 variaveis para uso, n1 é n2, nelas contem dados do meio subtraindo com fim + 1, é e meio - fim
+	int vetesquerdo[n1]; // Armazena o lado esquerdo do vetor ja dividido
+        int vetdireito[n2];  // Armazena o lado direito do vetor tambem ja ordenando
 
         for(int i = 0; i < n1; i++) vetesquerdo[i] = vetor[inicio + 1];
         for(int j = 0; j < n2; j++) vetdireito[j] = vetor[meio+1+j]; 
@@ -13,15 +17,15 @@ void merge_sort(int vetor[], int inicio, int meio, int fim){
         int i = 0, j = 0, k = inicio;
 	while(i < n1 && j < n2){
   
-           if (vetesquerdo[i] <= vetdireito[j]){
-	        vetor[k] = vetesquerdo[i++];
+                 if (vetesquerdo[i] <= vetdireito[j]){
+	          vetor[k] = vetesquerdo[i++];
               
-           }else{
-                vetor[k] = vetdireito[j++];
+                }else{
+                  vetor[k] = vetdireito[j++];
     
-         }// fim do if 
+                }// fim do if 
 
-  }//Fim do while
+        }//Fim do while
    	k++;
 	
     	while(i < n1) vetor[k++] = vetesquerdo[i++];
