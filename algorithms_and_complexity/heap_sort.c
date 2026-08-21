1529f95c50bd853b27337f4ad36f980583716385
carlos daniel <cd38668cd@gmail.com>
2026-08-20 21:23:43 -0300

    Finalizando funçao principal

--- algorithms_and_complexity/heap_sort.c
+++ algorithms_and_complexity/heap_sort.c
@@ -15,13 +15,28 @@ void heap_fuc(int vetor[], int n, int i){
 	int dire = 2*i+2; //indice do filha da direita
 
        	//Verifica se o filho da esquerda e maior que a raiz
-       	if(esqu < n && vetor[esqu] > vetor[meior]) maior = esqu;
+       	if(esqu < n && vetor[esqu] > vetor[maior]) maior = esqu;
 	//Verifica se o filho direito existe ou e maior que a raiz
-	if(dire < n && vetor[dire] > vetor[meior]) meior = dire;
+	if(dire < n && vetor[dire] > vetor[maior]) maior = dire;
        
  	// Se o maior não for a raiz, troca e continua a "consertar" para baixo
 	if(maior != i){
-	    trocar(&vetor[i], &trocar[maior]);
+	    trocar(&vetor[i], &vetor[maior]);
             heap_fuc(vetor, n, maior);	
 	}//fim do if
 }// fim da funçâo heap_fy
+
+void heap_sort(int vetor[], int n){
+	//Construindo o heap (reorganizando o vetor)
+	for(int i = n/2 - 1; i >= 0; i--){
+		heap_fuc(vetor, n, i);
+	}//fim do for
+
+	//Um por um extrai um elemento do heap
+	for(int i = n-1; i >= 0; i--){
+		//Move a raiz atual para o final
+		trocar(&vetor[0], &vetor[i]);
+		//Chama heapify no heap reduzido
+		heap_fuc(vetor, i, 0);
+	}//fim do for
+}//fim da funçâo heap_sort
