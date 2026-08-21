66ff8c773934e370ec5b4a75e299de52969d6377
carlos daniel <cd38668cd@gmail.com>
2026-08-20 21:16:12 -0300

    Verificâo da raiz e seus filhos

--- algorithms_and_complexity/heap_sort.c
+++ algorithms_and_complexity/heap_sort.c
@@ -2,3 +2,21 @@
 
 //Iniciando jeap sort um dosmelhores algoritmos em questâo de desenpenho e espaço
 
+void trocar(int *a, int* b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}//fim da funçâo trocar
+
+
+void heap_fuc(int vetor[], int n, int i){
+	int maior = i; //Iniicalizando maior como raiz da subarvore
+	int esqu = 2*i+1; //Indice do filho da esquerda
+	int dire = 2*i+2; //indice do filha da direita
+
+       	//Verifica se o filho da esquerda e maior que a raiz
+       	if(esqu < n && vetor[esqu] > vetor[meior]) maior = esqu;
+	//Verifica se o filho direito existe ou e maior que a raiz
+	if(dire < n && vetor[dire] > vetor[meior]) meior = dire;
+
+}// fim da funçâo heap_fy
