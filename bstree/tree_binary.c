0bd538d90e4a6b9f03187be176759f7095368a2a
carlos daniel <cd38668cd@gmail.com>
2026-08-23 09:22:21 -0300

    Inciando arvores binaria de busca

---
+++ bstree/tree_binary.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Iniciando arvores binarias e arvores de busca neste diretorio
+  Contém codigos de exemplos funcionais para esssa estrutura de dados
+    
+*/
