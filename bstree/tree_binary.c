7ca2666df45122b913d0f9d3a86760c708c857b0
carlos daniel <cd38668cd@gmail.com>
2026-08-23 09:25:58 -0300

    Struct inicial para representa o no ou root

--- bstree/tree_binary.c
+++ bstree/tree_binary.c
@@ -3,5 +3,13 @@
 
 /*Iniciando arvores binarias e arvores de busca neste diretorio
   Contém codigos de exemplos funcionais para esssa estrutura de dados
-    
+
 */
+
+//Neste struct contém uma variavl do tipo inteira para os dados
+//É dois ponteiro que sera para no filhos direito e esquerdo
+struct Node {
+    int data;
+    struct Node* esquerda;
+    struct Node* direita;
+};
