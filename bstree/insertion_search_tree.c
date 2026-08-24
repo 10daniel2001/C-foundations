e96ec01409e1a32b61ded14208e72363c118efb2
carlos daniel <cd38668cd@gmail.com>
2026-08-24 18:34:35 -0300

    Update

--- bstree/insertion_search_tree.c
+++ bstree/insertion_search_tree.c
@@ -108,7 +108,7 @@ arvore interative_insertion(arvore raiz, int valor){
         }
         
     
-    return novo;
+    return raiz;
 
 }
 
@@ -120,4 +120,43 @@ void free_arvore(arvore raiz){
     free(raiz);
     //Libera a memória alocada para a árvore
     //Frees the memory allocated for the tree
+}
+
+void em_ordem(arvore raiz){
+    if (raiz == NULL) return;
+    em_ordem(raiz->esquerda);
+    printf("No pai--%d--\n", raiz->data);
+    em_ordem(raiz->direita);
+}
+
+void visualizar(){
+    arvore arvore1 = NULL;
+
+    int valores_balanceados[] = {50, 30, 90, 20, 40, 100};
+    int n1 = sizeof(valores_balanceados) / sizeof(valores_balanceados[0]);
+
+    for (int i = 0; i < n1; i++)
+    {
+        arvore1 = insertion_recursive(arvore1, valores_balanceados[i]);
+    }
+
+    arvore arvore2 = NULL;
+    int valores_crescentes[] = {20, 30, 40, 50, 90, 100};
+    int n2 = sizeof(valores_crescentes) / sizeof(valores_crescentes[0]);
+    for (int i = 0; i < n2; i++) {
+        arvore2 = insertion_recursive(arvore2, valores_crescentes[i]);
+    }
+    printf("---------------------\n");
+    em_ordem(arvore1);
+    printf("------------------------\n");
+    em_ordem(arvore2);
+
+    free_arvore(arvore1);
+    free_arvore(arvore2);
+}
+
+int main(){
+
+    visualizar();
+  
 }
