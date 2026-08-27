133b468701999f92549f334c1bde10bc16d82116
carlos daniel <cd38668cd@gmail.com>
2026-08-27 17:52:01 -0300

    Iniciando DSW algoritmo de balancear arvores in-place

---
+++ bstree/dsw_tree.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*fase 01
+  Criação da vine (espinha): transforma a árvore em uma lista ligada linear (vine) exclusivamente com filhos
+  à direita, utilizando rotações à direita
+
+  fase 02
+  Balanceamento: converte a vine em uma árvore perfeitamente balanceada (ou quase balanceada, 
+  dependendo do número de nós) por meio de rotações controladas à esquerda.
+*/
+
+
+// O codigo e o mesmo dos outros arquivos 
