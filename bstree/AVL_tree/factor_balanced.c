98a0810b5327613d2acbf603b0e68c283ad8cc69
carlos daniel <cd38668cd@gmail.com>
2026-08-31 17:44:14 -0300

    atualizar altura

--- bstree/AVL_tree/factor_balanced.c
+++ bstree/AVL_tree/factor_balanced.c
@@ -29,3 +29,14 @@ int fator_balanced(NoAvl *no){
     return fator_balanced(no->esquerdo) - fator_balanced(no->direito);
     
 }
+// Funçao para atualizar a altura de num nó
+void atualizar_altura(NoAvl* no){
+    if (no != NULL)
+    {
+        int altura_esquerda = altura(no->esquerdo);
+        int altura_direita = altura(no->direito);
+        no->altura = 1 + (altura_esquerda > altura_direita ? altura_esquerda : altura_direita);
+        no->fator_balanceamento = fator_balanced(no); 
+    }
+    
+}
