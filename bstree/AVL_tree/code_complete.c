22198ec4bca3b1b3e75bb9ed3a9e291247fd7e09
carlos daniel <cd38668cd@gmail.com>
2026-09-02 15:54:01 -0300

    Função para retorna a altura da árvore

--- bstree/AVL_tree/code_complete.c
+++ bstree/AVL_tree/code_complete.c
@@ -12,3 +12,25 @@ typedef struct Node
 }Node;
 
 typedef Node* ArvaoreAvl;
+
+Node criar_no(int valor){
+   Node* novo = (Node*)malloc(sizeof(Node));
+    if (novo == NULL)
+    {
+        printf("Falha ao criar nó\n");
+        return ;
+    }
+    novo->dados = valor;
+    novo->esquerda = NULL;
+    novo->direita = NULL;
+}
+
+int altura_no(Node *no){
+    if (no == NULL)
+    {
+        printf("Erro de analisar altura\n");
+        return;
+    }
+    return no->altura;
+    
+}
