1a76e1e2a9d9a1c22f967d7c1585081ea4a50e11
carlos daniel <cd38668cd@gmail.com>
2026-09-07 13:25:54 -0300

    Macros basica em C

---
+++ 07_playground/macros_in_C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/*Conceitos de macros em C
+
+macros são criadas usando o pré-processador,
+através da diretiva #define. O pré-processador substitui 
+o texto da macro no código antes da compilação propriamente dita
+
+
+*/
+
+//Macro basica 
+#define PI 3.14159
+#define TAMANHO 50
+
