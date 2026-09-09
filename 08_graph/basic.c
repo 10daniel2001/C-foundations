75555674e56d76252530b1ded85bf35728616dc0
carlos daniel <cd38668cd@gmail.com>
2026-09-08 22:56:17 -0300

    Grafós basicos

---
+++ 08_graph/basic.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define Tamm 5
+
+
+void grafos(){
+
+    int graphh[Tamm][Tamm] = {0};
+
+    graphh[0][1] = 1;
+    graphh[0][0] = 0;
+    graphh[1][2] = 1;
+    graphh[2][1] = 1;
+    graphh[4][3] = 0;
+
+    for (int i = 0; i < Tamm; i++)
+    {
+        for (int j = 0; j < Tamm; j++)
+        {
+            printf("%d ", graphh[i][j]);
+        }
+        
+        printf("\n");
+    }
+    
+
+
+
+}
+
+int main(){
+
+    grafos();
+
+    return 0;
+}
