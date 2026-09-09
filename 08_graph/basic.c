8894f7134197ae4926e87515e5639a9e714da508
carlos daniel <cd38668cd@gmail.com>
2026-09-08 23:01:08 -0300

    Grafós funçao matriz de exomplo

--- 08_graph/basic.c
+++ 08_graph/basic.c
@@ -9,12 +9,21 @@ void grafos(){
     // Matriz do tipo inteiro para representar 
     int graphh[Tamm][Tamm] = {0};
 
+    //Exemplo de uma matriz
+    for(int g = 0; g < Tamm; g++){
+        for (int i = 0; i < Tamm; i++)
+        {
+            printf("%d ", graphh[g][i]);
+        }
+        printf("\n");
+    }
+
     graphh[0][1] = 1;
     graphh[0][0] = 0;
     graphh[1][2] = 1;
     graphh[2][1] = 1;
     graphh[4][3] = 0;
-
+printf("\n");
     for (int i = 0; i < Tamm; i++)
     {
         for (int j = 0; j < Tamm; j++)
