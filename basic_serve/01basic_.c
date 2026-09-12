616cc84f2bd08df5941aeac01bb3693cd0fae317
carlos daniel <cd38668cd@gmail.com>
2026-09-12 19:12:00 -0300

    Uma boa explicada

--- basic_serve/01basic_.c
+++ basic_serve/01basic_.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <stdio.h> // Biblioteca padrao de entradada e saida 
+#include <stdlib.h> // Para o uso do free, ou allocar memoria
+#include <string.h> // Para uso de funçoes para strings
+#include <unistd.h> // Para uso de sistemas unix, linux 
+#include <sys/socket.h> // Para configurar socket 
+#include <arpa/inet.h> // Para traduzir endereços 
 
+// Porta padrão deste uso e a 8080, póis nao e necessario permisssao de super usuiario por hora !
 #define PORTA 8080
+// Tamnho do buffer de caracters é 1024, isso é 1023, para caracters do cliente !
 #define TAM_BUFFER  1024
 
 int main(){
     int fd_serve, fd_cliente;
     char buffer[TAM_BUFFER];
-    int tamanho_endereco = sizeof(tamanho_endereco);
+    struct sockaddr* endereco;
+    int tamanho_endereco = sizeof(endereco);
     char *msg_boas_vinda = "Ola, Você esta concetado";
-    
+
 }
