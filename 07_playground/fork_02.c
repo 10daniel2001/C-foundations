a7da5b6af8e17ed7b5fa023cc76bf220a6e56b03
carlos daniel <cd38668cd@gmail.com>
2026-09-11 21:55:39 -0300

    Iniciando comentarios

--- 07_playground/fork_02.c
+++ 07_playground/fork_02.c
@@ -15,15 +15,19 @@ if (resultado < 0)
 printf("Algo deu errado!!!\n");
 
 pid = getpid();
+// Obtém o PID do processo atual usando a função getpid().
 
 if (resultado == 0) {
-
+// Se o valor de retorno for igual a 0, significa que estamos no processo filho.
 ppid = getppid();
+// Obtém o PID do processo pai usando a função getppid().
 printf("Eu sou o processo filho, meu PID é %d e meu pai tem PID=%d.\n", pid, ppid);
 }
 if (resultado > 0) {
+// Se o valor de retorno for maior que 0, significa que estamos no processo pai.
 printf("Eu sou o processo pai, meu PID é %d e meu filho tem PID=%d.\n", pid, resultado);
 waitpid(resultado, NULL, 0);
+// Aguarda o término do processo filho usando a função waitpid(). O primeiro argumento é o PID do processo filho, o segundo argumento é um ponteiro para uma variável que armazenará o status de término do processo filho (neste caso, NULL), e o terceiro argumento é um conjunto de opções (neste caso, 0).
 }
 
 return 0;
